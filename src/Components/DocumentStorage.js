eff28bb5b9579b4ed79bd3162f9b4dd82b830e54
Taufiq <taufiq9.2004@gmail.com>
2024-05-25 22:43:25 +0530

    UI upgrade doc storage

--- src/Components/DocumentStorage.js
+++ src/Components/DocumentStorage.js
@@ -159,8 +159,54 @@ const DocumentStorage = () => {
         </Dialog>
       </header>
       <div>
-        <input type="file" onChange={onFileChange} />
-        <button onClick={submitForm}>Upload</button>
+        <div class="flex items-center justify-center w-1/2 m-auto my-10">
+          <label
+            for="dropzone-file"
+            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-indigo-200  hover:bg-gray-100 dark:border-gray-600 dark:hover:bg-indigo-200  dark:hover:text-black"
+          >
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                />
+              </svg>
+              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span class="font-semibold">Click to upload</span> or drag and
+                drop
+              </p>
+              <p class="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input
+              onChange={onFileChange}
+              id="dropzone-file"
+              type="file"
+              class="hidden"
+            />
+          </label>
+        </div>
+        <div className="flex justify-center items-center">
+          <button
+            type="button"
+            onClick={submitForm}
+            className=" w-36 m-4 rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            Upload
+          </button>
+        </div>
+
+        {/* <input className="" type="file" onChange={onFileChange} /> */}
       </div>
 
       <footer className="bg-indigo-200 text-black py-12 mt-20">
